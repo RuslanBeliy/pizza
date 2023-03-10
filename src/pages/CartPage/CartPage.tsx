15a4439a0b6eb6281682c05bbb056cbc794bc7e1
Ruslan Beliy <ruslan.beliy98@gmail.com>
2023-03-10 18:39:29 +0300

    fixed scroll cartPage

--- src/pages/CartPage/CartPage.tsx
+++ src/pages/CartPage/CartPage.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 
 import s from './CartPage.module.scss';
 
@@ -9,6 +9,10 @@ interface Props {}
 
 export const CartPage: FC<Props> = () => {
   const countPizzas = useAppSelector((state) => state.cart.countPizzas);
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
   return (
     <Container className={s.cartPage}>
       <Title className={s.title} tag='h2' variant='fz32'>
